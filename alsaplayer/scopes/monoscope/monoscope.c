9217a59324c74a58b9cc942fe8308e77eb693011
adnans <adnans@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2001-09-04 11:57:42 +0000

    Disabled convolve routine since it contains a bug. Will investigate this further.

--- alsaplayer/scopes/monoscope/monoscope.c
+++ alsaplayer/scopes/monoscope/monoscope.c
@@ -33,17 +33,19 @@
 #include "convolve.h"
 
 short newEq[CONVOLVE_BIG];	// latest block of 512 samples.
+static short copyEq[CONVOLVE_BIG];
 int avgEq[CONVOLVE_SMALL];	// a running average of the last few.
 int avgMax;			// running average of max sample.
 
-static GdkImage *image = NULL;
+static GtkWidget *area = NULL;
 static GtkWidget *scope_win = NULL;
+static GdkRgbCmap *color_map = NULL;
 static int ready_state = 0;
 static pthread_t monoscope_thread;
 static pthread_mutex_t monoscope_mutex;
 static int is_init = 0;
 static int running = 0;
-static convolve_state *state;
+static convolve_state *state = NULL;
 
 static void monoscope_hide();
 
@@ -70,277 +72,94 @@ void monoscope_set_data(void *audio_buffer, int size)
 	}
 }
 
-static	short copyEq[512];
 
-#define MONOSCOPE_DOLOOP() \
-while (running) { \
-	int w, factor;\
-	int max = 1;\
-	short * thisEq; \
-	memcpy (copyEq, newEq, sizeof (short) * CONVOLVE_BIG); \
-	thisEq = copyEq + convolve_match (avgEq, copyEq, state); \
-	for (w=0; w < 256 * 128; w++) { \
-		bits[w] = bg_color.pixel; \
-	} \
-\
-	for (i=0; i < 256; i++) { \
-		foo = thisEq[i] + (avgEq[i] >> 1); \
-		avgEq[i] = foo; \
-		if (foo < 0) \
-			foo = -foo; \
-		if (foo > max) \
-			max = foo; \
-	} \
-        avgMax += max - (avgMax >> 8); \
-	if (avgMax < max) \
-		avgMax = max; /* Avoid overflow */ \
-    factor = 0x7fffffff / avgMax; \
-	/* Keep the scaling sensible. */ \
-	if (factor > (1 << 18)) \
-		factor = 1 << 18; \
-	if (factor < (1 << 8)) \
-		factor = 1 << 8; \
-	for (i=0; i < 256; i++) { \
-		foo = avgEq[i] * factor; \
-                foo >>= 18; \
-		if (foo > 63) \
-			foo = 63; \
-		if (foo < -64) \
-			foo = -64; \
-		bar = (i + ((foo+64) << 8)); \
-		if ((bar > 0) && (bar < (256 * 128))) { \
-			loc = bits + bar; \
-			if (foo < 0) { \
-				for (h = 0; h <= (-foo); h++) { \
-					*loc = colEq[h]; \
-					loc+=256; \
-				} \
-			} else { \
-				for (h = 0; h <= foo; h++) { \
-					*loc = colEq[h]; \
-					loc-=256; \
-				} \
-			} \
-		} \
-	} \
-	for (i=16;i < 128; i+=16) { \
-		for (h = 0; h < 256; h+=2) { \
-			bits[(i << 8) + h] = raster_color.pixel; \
-		} \
-	} \
-	for (i = 16; i < 256; i+=16) { \
-		for (h = 0; h < 128; h+=2) { \
-			bits[i + (h << 8)] = raster_color.pixel; \
-		} \
-	} \
-	GDK_THREADS_ENTER(); \
-	gdk_draw_image(win,gc,image,0,0,0,0,-1,-1); \
-	gdk_flush(); \
-	GDK_THREADS_LEAVE(); \
-	dosleep(SCOPE_SLEEP); \
-}
-
-void monoscope32(void *data)
+void the_monoscope()
 {
-	guint32 *loc;
-	gint foo;
-	guint32 *bits;
+	int foo;
 	int bar;  
-	guint32 colEq[65];
 	int i, h;
-	GdkWindow *win;
-	GdkColormap *c;
-	GdkVisual *v;
-	GdkGC *gc;
-	GdkColor raster_color;
-	GdkColor bg_color;
+	guchar bits[ 257 * 129];
+  guchar *loc;
 	
-	win = (GdkWindow *)data;
-	GDK_THREADS_ENTER();
-	c = gdk_colormap_get_system();
-	gc = gdk_gc_new(win);
-	v = gdk_window_get_visual(win);
-
-	for (i = 0; i < 32; i++) {
-		GdkColor color;
-		color.red = (i*8) << 8;
-		color.green = 255 << 8;
-		color.blue = 0;
-	        gdk_color_alloc(c, &color);
-		colEq[i] = color.pixel; 
-		color.red = 255 << 8;
-		color.green = ((31 - i) * 8) << 8;
-		color.blue = 0;
-		gdk_color_alloc(c, &color);
-		colEq[i + 32] = color.pixel;
-  	}
-	raster_color.red = 40 << 8;
-	raster_color.green = 75 << 8;
-	raster_color.blue = 0;
-	gdk_color_alloc(c, &raster_color);
-
-	// Create render image
-	if (image) {
-		gdk_image_destroy(image);
-		image = NULL;
+	running = 1;
+				
+	while (running) {
+					int w, factor;
+					int val;
+					int max = 1;
+					short * thisEq;
+					memcpy (copyEq, newEq, sizeof (short) * CONVOLVE_BIG);
+					//val = convolve_match (avgEq, copyEq, state);
+					//printf("val = %d\n", val);
+					thisEq =copyEq;
+#if 0					
+					thisEq = copyEq + convolve_match (avgEq, copyEq, state);
+#endif					
+					memset(bits, 0, 256 * 128);
+					for (i=0; i < 256; i++) {
+									foo = thisEq[i] + (avgEq[i] >> 1);
+									avgEq[i] = foo;
+									if (foo < 0)
+													foo = -foo;
+									if (foo > max)
+													max = foo;
+					}
+					avgMax += max - (avgMax >> 8);
+					if (avgMax < max)
+									avgMax = max; /* Avoid overflow */
+					factor = 0x7fffffff / avgMax;
+					/* Keep the scaling sensible. */
+					if (factor > (1 << 18))
+									factor = 1 << 18;
+					if (factor < (1 << 8))
+									factor = 1 << 8;
+					for (i=0; i < 256; i++) {
+									foo = avgEq[i] * factor;
+									foo >>= 18;
+									if (foo > 63)
+													foo = 63;
+									if (foo < -64)
+													foo = -64;
+									val = (i + ((foo+64) << 8));
+									bar = val;
+									if ((bar > 0) && (bar < (256 * 128))) {
+													loc = bits + bar;
+													if (foo < 0) {
+																	for (h = 0; h <= (-foo); h++) {
+																					*loc = h;
+																					loc+=256; 
+																	}
+													} else {
+																	for (h = 0; h <= foo; h++) {
+																					*loc = h;
+																					loc-=256;
+																	}
+													}
+									}
+					}
+					for (i=16;i < 128; i+=16) {
+									for (h = 0; h < 256; h+=2) {
+													bits[(i << 8) + h] = 64;
+									}
+					}
+					for (i = 16; i < 256; i+=16) {
+									for (h = 0; h < 128; h+=2) {
+													bits[i + (h << 8)] = 64;
+									}
+					}
+					GDK_THREADS_ENTER();
+					gdk_draw_indexed_image(area->window,area->style->white_gc,
+													0, 0, 256, 128, GDK_RGB_DITHER_NONE, bits, 256, color_map);
+					gdk_flush();
+					GDK_THREADS_LEAVE();
+					dosleep(SCOPE_SLEEP);
 	}
-	image = gdk_image_new(GDK_IMAGE_FASTEST, v, 256, 128);
-	bg_color.red = SCOPE_BG_RED << 8;
-	bg_color.green = SCOPE_BG_GREEN << 8;
-	bg_color.blue = SCOPE_BG_BLUE << 8;
-        gdk_color_alloc(c, &bg_color); 
-	GDK_THREADS_LEAVE();
-	
-	assert(image);
-	assert(image->bpp > 2);
-	
-	bits = (guint32 *)image->mem;	
-
-	running = 1;
-
-	MONOSCOPE_DOLOOP();
-	
-	GDK_THREADS_ENTER();
-	monoscope_hide();
-	GDK_THREADS_LEAVE();
-}
-
-
-
-void monoscope16(void *data)
-{
-	guint16 *loc;
-	gint foo;
-	guint16 *bits;
-	int bar;  
-	guint16 colEq[65];
-	int i, h;
-	GdkWindow *win;
-	GdkColormap *c;
-	GdkVisual *v;
-	GdkGC *gc;
-	GdkColor raster_color;
-	GdkColor bg_color;
-	
-	win = (GdkWindow *)data;
-	GDK_THREADS_ENTER();
-	c = gdk_colormap_get_system();
-	gc = gdk_gc_new(win);
-	v = gdk_window_get_visual(win);
-
-	for (i = 0; i < 32; i++) {
-		GdkColor color;
-		color.red = (i*8) << 8;
-		color.green = 255 << 8;
-		color.blue = 0;
-	        gdk_color_alloc(c, &color);
-		colEq[i] = color.pixel; 
-		color.red = 255 << 8;
-		color.green = ((31 - i) * 8) << 8;
-		color.blue = 0;
-		gdk_color_alloc(c, &color);
-		colEq[i + 32] = color.pixel;
-  	}
-	raster_color.red = 40 << 8;
-	raster_color.green = 75 << 8;
-	raster_color.blue = 0;
-	gdk_color_alloc(c, &raster_color);
-
-	// Create render image
-	if (image) {
-		gdk_image_destroy(image);	
-		image = NULL;
-	}
-	image = gdk_image_new(GDK_IMAGE_FASTEST, v, 256, 128);
-	bg_color.red = SCOPE_BG_RED << 8;
-	bg_color.green = SCOPE_BG_GREEN << 8;
-	bg_color.blue = SCOPE_BG_BLUE << 8;        
-	gdk_color_alloc(c, &bg_color); 
-	GDK_THREADS_LEAVE();
-	
-	assert(image);
-	assert(image->bpp == 2);
-	
-	bits = (guint16 *)image->mem;	
-	
-	running = 1;
-
-	MONOSCOPE_DOLOOP();
 
 	GDK_THREADS_ENTER();
 	monoscope_hide();
 	GDK_THREADS_LEAVE();
 }
 
-
-void monoscope8(void *data)
-{
-	guint8 *loc;
-	gint8 foo;
-	guint8 colEq[65];
-	guint8 *bits;
-	int bar;  
-	int i, h;
-	GdkWindow *win;
-	GdkColormap *c;
-	GdkVisual *v;
-	GdkGC *gc;
-	GdkColor raster_color;
-	GdkColor bg_color;
-	
-	win = (GdkWindow *)data;
-	GDK_THREADS_ENTER();
-	c = gdk_colormap_get_system();
-	gc = gdk_gc_new(win);
-	v = gdk_window_get_visual(win);
-
-	for (i = 0; i < 32; i++) {
-		GdkColor color;
-		color.red = (i*8) << 8;
-		color.green = 255 << 8;
-		color.blue = 0;
-	        gdk_color_alloc(c, &color);
-		colEq[i] = color.pixel; 
-		color.red = 255 << 8;
-		color.green = ((31 - i) * 8) << 8;
-		color.blue = 0;
-		gdk_color_alloc(c, &color);
-		colEq[i + 32] = color.pixel;
-  	}
-	
-	raster_color.red = 40 << 8;
-	raster_color.green = 75 << 8;
-	raster_color.blue = 0;
-	gdk_color_alloc(c, &raster_color);
-	
-	// Create render image
-	if (image) {
-		gdk_image_destroy(image);
-		image = NULL;
-	}	
-	image = gdk_image_new(GDK_IMAGE_FASTEST, v, 256, 128);
-	bg_color.red = SCOPE_BG_RED << 8;
-	bg_color.green = SCOPE_BG_GREEN << 8;
-	bg_color.blue = SCOPE_BG_BLUE << 8;        
-	gdk_color_alloc(c, &bg_color); 
-	GDK_THREADS_LEAVE();
-	
-	assert(image);
-	assert(image->bpp == 1);
-	
-	bits = (guint8 *)image->mem;	
-
-	running = 1;
-
-	MONOSCOPE_DOLOOP();
-
-	GDK_THREADS_ENTER();
-	monoscope_hide();
-	GDK_THREADS_LEAVE();
-}
-
-static GdkVisual *visual;
-static GdkWindow *win;
 
 static void test_cb(GtkWidget *widget, gpointer data)
 {
@@ -352,8 +171,10 @@ void stop_monoscope();
 
 static gboolean close_monoscope_window(GtkWidget *widget, GdkEvent *event, gpointer data)
 {
-        stop_monoscope();
-
+		GDK_THREADS_LEAVE();		
+		stop_monoscope();
+		GDK_THREADS_ENTER();
+		
 		return TRUE;
 }
 
@@ -361,40 +182,46 @@ static gboolean close_monoscope_window(GtkWidget *widget, GdkEvent *event, gpoin
 GtkWidget *init_monoscope_window()
 {
 	GtkWidget *monoscope_win;
-	GtkStyle *style;
-	GdkColor *color;
+	GdkColor color;
+	guint32 colors[65];
+	int i;
 	
 	pthread_mutex_init(&monoscope_mutex, NULL);
 
-	style = gtk_style_new();
 	monoscope_win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(monoscope_win), "AlsaPlayer: Monoscope");
 	gtk_widget_set_usize(monoscope_win, 256,128);
 	gtk_window_set_wmclass (GTK_WINDOW(monoscope_win), "Monoscope", "AlsaPlayer");
 	gtk_window_set_policy (GTK_WINDOW (monoscope_win), FALSE, FALSE, FALSE);
-	style = gtk_style_copy(gtk_widget_get_style(GTK_WIDGET(monoscope_win)));
 	
-	color = &style->bg[GTK_STATE_NORMAL];
-	color->red = SCOPE_BG_RED << 8;
-	color->blue = SCOPE_BG_BLUE << 8;
-	color->green = SCOPE_BG_GREEN << 8;
-	gdk_color_alloc(gdk_colormap_get_system(), color);
-	gtk_widget_set_style(GTK_WIDGET(monoscope_win), style);
+	gtk_widget_realize(monoscope_win);
 	
+	color.red = SCOPE_BG_RED << 8;
+	color.blue = SCOPE_BG_BLUE << 8;
+	color.green = SCOPE_BG_GREEN << 8;
+	gdk_color_alloc(gdk_colormap_get_system(), &color);
+
+	colors[0] = 0;
+	for (i = 1; i < 32; i++) {
+		GdkColor color;
+		colors[i] = (i*8 << 16) +(255 << 8);
+		colors[i+32] = (255 << 16) + (((31 - i) * 8) << 8);
+  	}
+	colors[64] = (40 << 16) + (75 << 8);
+	color_map = gdk_rgb_cmap_new(colors, 65);
+	area = gtk_drawing_area_new();
+	gtk_container_add(GTK_CONTAINER(monoscope_win), area);
+	gtk_widget_realize(area);
+	gdk_window_set_background(area->window, &color);
+
+	gtk_widget_show(area);
 	gtk_widget_show(monoscope_win);
 	
-	win = monoscope_win->window;
-
 	// Signals
 		
 	gtk_signal_connect(GTK_OBJECT(monoscope_win), "delete_event",
                 GTK_SIGNAL_FUNC(close_monoscope_window), monoscope_win);
 
-
-	// Clear and show the window
-	gdk_window_clear(win);
-	gdk_window_show(win);
-	gdk_flush(); 
 
 	ready_state = 1;
 
@@ -410,39 +237,27 @@ void monoscope_hide()
 		gdk_window_get_root_origin(scope_win->window, &x, &y);
 		gtk_widget_hide(scope_win);
 		gtk_widget_set_uposition(scope_win, x, y);
-	}
+	} else {
+		printf("Tried to hide destroyed widget!\n");
+	}	
 }
 
 
 void stop_monoscope()
 {
 	running = 0;
+	pthread_join(monoscope_thread, NULL);
 }
 
 
 void run_monoscope(void *data)
 {
 	nice(SCOPE_NICE); // Be nice to most processes
-
-	GDK_THREADS_ENTER();	
-	visual = gdk_window_get_visual(win); 
-	GDK_THREADS_LEAVE();
-
-	switch (visual->depth) {
-	 case 8:
-		monoscope8(win);
-		break;
-	 case 16:
-		monoscope16(win);
-		break;
-	 case 24:
-	 case 32:
-		monoscope32(win);
-		break;
-		
-	}
-	//delete sub;
+	
+	the_monoscope();
+	
 	pthread_mutex_unlock(&monoscope_mutex);
+	pthread_exit(NULL);
 }
 
 
@@ -458,7 +273,6 @@ void start_monoscope(void *data)
 	}
 	gtk_widget_show(scope_win);
 	pthread_create(&monoscope_thread, NULL, (void * (*)(void *))run_monoscope, data);
-	pthread_detach(monoscope_thread);
 }
 
 
@@ -480,6 +294,18 @@ static int init_monoscope()
 
 static void close_monoscope()
 {
+		if (state)
+						convolve_close(state);		
+/*				
+	if (image) {
+					gdk_image_destroy(image);
+					image = NULL;
+	}				
+	if (scope_win) {
+					gtk_widget_destroy(scope_win);
+					scope_win = NULL;
+	}
+*/	
 }
 
 static int monoscope_running()
