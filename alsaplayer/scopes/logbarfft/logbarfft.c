617c6f0ad19479221bfe5f9022cd0c3392c1f6d9
adnans <adnans@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2001-11-01 03:26:57 +0000

    Rewrote the logbarfft to use indexed images. The code is actually readable now. File size went from 10 to 6K!

--- alsaplayer/scopes/logbarfft/logbarfft.c
+++ alsaplayer/scopes/logbarfft/logbarfft.c
@@ -33,6 +33,8 @@
 
 #define BARS 32
 
+static GtkWidget *area = NULL;
+static GdkRgbCmap *color_map = NULL;
 static sound_sample actEq[FFT_BUFFER_SIZE];
 static sound_sample oldEq[FFT_BUFFER_SIZE];
 static int maxbar[BARS];
@@ -82,232 +84,58 @@ void logscope_set_data(void *audio_buffer, int size)
 	}
 }
 
-#define FFTSCOPE_DOLOOP() \
-while (running) { \
-    guint val;\
-    gint j; \
-    gint k; \
-	gint w; \
-\
-    for (w=0; w < 256 * 128; w++) { \
-	bits[w] = bg_color.pixel; \
-    } \
-\
-    /*memset(bits,0x0, 256 * 128 * image->bpp);*/ \
-    memcpy(&oldEq, &actEq, sizeof(actEq)); \
-\
-    fft_perform(oldEq, fftout, fftstate); \
-\
-    for (i=0; i < BARS; i++) { \
-	val = 0; \
-	for (j = xranges[i]; j < xranges[i + 1]; j++) { \
-		k = (guint)(sqrt(fftout[j]) * fftmult);\
-		val += k;\
-	} \
-	if(val > 127) val = 127; \
-	if (val > maxbar[ i ]) \
-		maxbar[ i ] = val; \
-	else { \
-		k = maxbar[ i ] - 6; \
-		val = k > 0 ? k : 0; \
-		maxbar[ i ] = val; \
-	} \
-	loc = bits + 256 * 127; \
-	for (h = val; h > 0; h--) {\
-	    for (j = (256 / BARS) * i + 0; j < (256 / BARS) * i + ((256 / BARS) - 1); j++) { \
-		*(loc + j) = colEq[val-h]; \
-	    } \
-	    loc -=256; \
-	} \
-    } \
-    GDK_THREADS_ENTER(); \
-    gdk_draw_image(win,gc,image,0,0,0,0,-1,-1); \
-    gdk_flush(); \
-    GDK_THREADS_LEAVE(); \
-    dosleep(SCOPE_SLEEP); \
-}
-
-static void fftscope32(void *data)
+static void the_fftscope()
 {
-	guint32 *loc;
-	guint32 *bits;
-	guint32 colEq[128];
+	guchar *loc;
+	guchar bits [256 * 129];
 	int i, h;
-	GdkWindow *win;
-	GdkColormap *c;
-	GdkVisual *v;
-	GdkGC *gc;
-	GdkColor bg_color;
 	
-	win = (GdkWindow *)data;
-	GDK_THREADS_ENTER();
-	c = gdk_colormap_get_system();
-	gc = gdk_gc_new(win);
-	v = gdk_window_get_visual(win);
-
-	for (i = 0; i < 64; i++) {
-		GdkColor color;
-		color.red = (i*4) << 8;
-		color.green = 255 << 8;
-		color.blue = 0;
-	        gdk_color_alloc(c, &color);
-		colEq[i] = color.pixel; 
-		color.red = 255 << 8;
-		color.green = ((63 - i) * 4) << 8;
-		color.blue = 0;
-		gdk_color_alloc(c, &color);
-		colEq[i + 64] = color.pixel;
-  	}
-
-	// Create render image
-	if (image) {
-		gdk_image_destroy(image);
-		image = NULL;
-	}
-	image = gdk_image_new(GDK_IMAGE_FASTEST, v, 256, 128);
-	bg_color.red = SCOPE_BG_RED << 8;
-	bg_color.green = SCOPE_BG_GREEN << 8;
-	bg_color.blue = SCOPE_BG_BLUE << 8;
-	gdk_color_alloc(c, &bg_color); 
-	GDK_THREADS_LEAVE();
-	
-	assert(image);
-	assert(image->bpp > 2);
-	
-	bits = (guint32 *)image->mem;	
-
 	running = 1;
 
-	FFTSCOPE_DOLOOP();
+	while (running) {
+    guint val;
+    gint j;
+    gint k;
+		gint w;
+   
+		memset(bits, 0, 256 * 128);
+    
+		memcpy(&oldEq, &actEq, sizeof(actEq));
 
-	GDK_THREADS_ENTER();
-	fftscope_hide();
-	GDK_THREADS_LEAVE();
-}
-
-
-
-static void fftscope16(void *data)
-{
-	guint16 *loc;
-	guint16 *bits;
-	guint16 colEq[128];
-	int i, h;
-	GdkWindow *win;
-	GdkColormap *c;
-	GdkVisual *v;
-	GdkGC *gc;
-	GdkColor bg_color;
-
-	win = (GdkWindow *)data;
-	GDK_THREADS_ENTER();
-	c = gdk_colormap_get_system();
-	gc = gdk_gc_new(win);
-	v = gdk_window_get_visual(win);
-
-	for (i = 0; i < 64; i++) {
-		GdkColor color;
-		color.red = (i*4) << 8;
-		color.green = 255 << 8;
-		color.blue = 0;
-	        gdk_color_alloc(c, &color);
-		colEq[i] = color.pixel; 
-		color.red = 255 << 8;
-		color.green = ((63 - i) * 4) << 8;
-		color.blue = 0;
-		gdk_color_alloc(c, &color);
-		colEq[i + 64] = color.pixel;
-  	}
-
-	// Create render image
-	if (image) {
-		gdk_image_destroy(image);	
-		image = NULL;
-	}
-	image = gdk_image_new(GDK_IMAGE_FASTEST, v, 256, 128);
-	bg_color.red = SCOPE_BG_RED << 8;
-	bg_color.green = SCOPE_BG_GREEN << 8;
-	bg_color.blue = SCOPE_BG_BLUE << 8;        
-	gdk_color_alloc(c, &bg_color); 
-	GDK_THREADS_LEAVE();
-
-	assert(image);
-	assert(image->bpp == 2);
-
-	bits = (guint16 *)image->mem;
-
-	running = 1;
-
-	FFTSCOPE_DOLOOP();
-
-	GDK_THREADS_ENTER();
-	fftscope_hide();
-	GDK_THREADS_LEAVE();
-}
-
-
-static void fftscope8(void *data)
-{
-	guint8 *loc;
-	guint8 colEq[128];
-	guint8 *bits;
-	int i, h;
-	GdkWindow *win;
-	GdkColormap *c;
-	GdkVisual *v;
-	GdkGC *gc;
-	GdkColor bg_color;
-	
-	win = (GdkWindow *)data;
-	GDK_THREADS_ENTER();
-	c = gdk_colormap_get_system();
-	gc = gdk_gc_new(win);
-	v = gdk_window_get_visual(win);
-
-	for (i = 0; i < 32; i++) {
-		GdkColor color;
-		color.red = (i*8) << 8;
-		color.green = 255 << 8;
-		color.blue = 0;
-	        gdk_color_alloc(c, &color);
-		colEq[i * 2] = color.pixel; 
-		colEq[i * 2 + 1] = color.pixel; 
-		color.red = 255 << 8;
-		color.green = ((31 - i) * 8) << 8;
-		color.blue = 0;
-		gdk_color_alloc(c, &color);
-		colEq[i * 2 + 64] = color.pixel;
-		colEq[i * 2 + 65] = color.pixel;
-  	}
-
-	// Create render image
-	if (image) {
-		gdk_image_destroy(image);
-		image = NULL;
+    fft_perform(oldEq, fftout, fftstate);
+    for (i=0; i < BARS; i++) { 
+				val = 0;
+				for (j = xranges[i]; j < xranges[i + 1]; j++) {
+					k = (guint)(sqrt(fftout[j]) * fftmult);
+					val += k;
+				}
+				if(val > 127) val = 127;
+				if (val > maxbar[ i ]) 
+					maxbar[ i ] = val;
+				else {
+					k = maxbar[ i ] - 6;
+					val = k > 0 ? k : 0;
+					maxbar[ i ] = val;
+				}
+				loc = bits + 256 * 128;
+				for (h = val; h > 0; h--) {
+	    		for (j = (256 / BARS) * i + 0; j < (256 / BARS) * i + ((256 / BARS) - 1); j++) {
+						*(loc + j) = val-h;
+	    		}
+	    		loc -=256;
+				}
+    	}
+    	GDK_THREADS_ENTER();
+			gdk_draw_indexed_image(area->window, area->style->white_gc,
+												0,0,256,128, GDK_RGB_DITHER_NONE, bits, 256, color_map);
+    	gdk_flush();
+    	GDK_THREADS_LEAVE();
+    	dosleep(SCOPE_SLEEP);
 	}	
-	image = gdk_image_new(GDK_IMAGE_FASTEST, v, 256, 128);
-	bg_color.red = SCOPE_BG_RED << 8;
-	bg_color.green = SCOPE_BG_GREEN << 8;
-	bg_color.blue = SCOPE_BG_BLUE << 8;        
-	gdk_color_alloc(c, &bg_color); 
-	GDK_THREADS_LEAVE();
-	
-	assert(image);
-	assert(image->bpp == 1);
-	
-	bits = (guint8 *)image->mem;	
-
-	running = 1;
-
-	FFTSCOPE_DOLOOP();
-
 	GDK_THREADS_ENTER();
 	fftscope_hide();
 	GDK_THREADS_LEAVE();
 }
-
-static GdkVisual *visual;
-static GdkWindow *win;
 
 
 static void stop_fftscope();
@@ -325,40 +153,44 @@ static gboolean close_fftscope_window(GtkWidget *widget, GdkEvent *event, gpoint
 static GtkWidget *init_fftscope_window()
 {
 	GtkWidget *fftscope_win;
-	GtkStyle *style;
-	GdkColor *color;
+	GdkColor color;
+	guint32 colors[129];
+	int i;
 	
 	pthread_mutex_init(&fftscope_mutex, NULL);
 
-	style = gtk_style_new();
 	fftscope_win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(fftscope_win), "logFFTscope");
 	gtk_widget_set_usize(fftscope_win, 256,128);
 	gtk_window_set_wmclass (GTK_WINDOW(fftscope_win), "logFFTscope", "AlsaPlayer");
 	gtk_window_set_policy (GTK_WINDOW (fftscope_win), FALSE, FALSE, FALSE);
-	style = gtk_style_copy(gtk_widget_get_style(GTK_WIDGET(fftscope_win)));
+
+	gtk_widget_realize(fftscope_win);
 	
-	color = &style->bg[GTK_STATE_NORMAL];
-	color->red = SCOPE_BG_RED << 8;
-	color->blue = SCOPE_BG_BLUE << 8;
-	color->green = SCOPE_BG_GREEN << 8;
-	gdk_color_alloc(gdk_colormap_get_system(), color);
-	gtk_widget_set_style(GTK_WIDGET(fftscope_win), style);
+	color.red = SCOPE_BG_RED << 8;
+	color.blue = SCOPE_BG_BLUE << 8;
+	color.green = SCOPE_BG_GREEN << 8;
+	gdk_color_alloc(gdk_colormap_get_system(), &color);
 	
+	colors[0] = 0;
+	for (i = 1; i < 64; i++) {
+		colors[i] = ((i*4) << 16) + (255 << 8);
+		colors[i + 63] = (255 << 16) + (((63 - i) * 4) << 8);
+  	}
+	color_map = gdk_rgb_cmap_new(colors, 128);
+	area = gtk_drawing_area_new();
+	gtk_container_add(GTK_CONTAINER(fftscope_win), area);
+	gtk_widget_realize(area);
+	gdk_window_set_background(area->window, &color);
+	
+	gtk_widget_show(area);
 	gtk_widget_show(fftscope_win);
 	
-	win = fftscope_win->window;
-
 	// Signals
 		
 	gtk_signal_connect(GTK_OBJECT(fftscope_win), "delete_event",
                 GTK_SIGNAL_FUNC(close_fftscope_window), fftscope_win);
 
-
-	// Clear and show the window
-	gdk_window_clear(win);
-	gdk_window_show(win);
-	gdk_flush(); 
 
 	ready_state = 1;
 
@@ -389,23 +221,8 @@ static void run_fftscope(void *data)
 {
 	nice(SCOPE_NICE); // Be nice to most processes
 
-	GDK_THREADS_ENTER();	
-	visual = gdk_window_get_visual(win); 
-	GDK_THREADS_LEAVE();
+	the_fftscope();
 
-	switch (visual->depth) {
-	 case 8:
-		fftscope8(win);
-		break;
-	 case 16:
-		fftscope16(win);
-		break;
-	 case 24:
-	 case 32:
-		fftscope32(win);
-		break;
-		
-	}
 	pthread_mutex_unlock(&fftscope_mutex);
 	pthread_exit(NULL);
 }
