e8d827f640a7b9b1cb68762ec6e2f0f1a1065df8
sjah <sjah@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2002-06-25 16:02:22 +0000

    Implement prebuffering.

--- alsaplayer/reader/http/http.c
+++ alsaplayer/reader/http/http.c
@@ -31,16 +31,34 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <pthread.h>
 
 #include "reader.h"
 #include "alsaplayer_error.h"
+#include "utilities.h"
 
 typedef struct http_desc_t_ {
     char *host, *path;
     int port;
     int sock;
     long size, pos;
+    void *buffer;
+    int begin;		    /* position for first byte of the buffer */
+    int len;		    /* length of the buffered data */
+    int direction;	    /* reading direction */
+    int going;		    /* true if buffer is filling */
+    pthread_t thread;	    /* thread which fill the buffer */
+    pthread_mutex_t buffer_lock;
+    pthread_mutex_t operation_lock;
+    pthread_cond_t read_condition;
+    int error;		    /* error status (0 - none) */
 } http_desc_t;
+
+/* How much data we should read at once? (bytes)*/
+#define  HTTP_BLOCK_SIZE  (32*1024)
+
+/* How much long showld be buffer? (bytes) */
+#define  HTTP_BUFFER_SIZE  (20000*1024)
 
 /* --------------------------------------------------------------- */
 /* ---------------------- NETWORK RELATED FUNCTIONS -------------- */
@@ -93,7 +111,8 @@ static int parse_uri (const char *uri, char **host, int *port, char **path)
     alsaplayer_error ("HTTP: Parsed URI: host='%s', port=%u, path='%s'\n", *host, *port, *path);
 
     return 0;
-}
+} /* end of: parse_uri */
+
 
 /* sleep for data */
 static int sleep_for_data (int sock)
@@ -112,7 +131,8 @@ static int sleep_for_data (int sock)
     }
 
     return 0;
-}
+} /* end of: sleep_for_data */
+
 
 /* get response head */
 static int get_response_head (int sock, char *response, int max)
@@ -130,10 +150,8 @@ static int get_response_head (int sock, char *response, int max)
 	if (sleep_for_data (sock))  return 1;
 	
 	/* read */
-	if (read (sock, response + len, 1) <=0 ) {
-	    alsaplayer_error ("HTTP: Error reading from socket.\n");
-	    return 1;
-	}
+	if (read (sock, response + len, 1) <=0 )
+	    break;
 	
 	len += 1;
     }
@@ -142,29 +160,76 @@ static int get_response_head (int sock, char *response, int max)
     response [len] = '\0';
     
     return 0;
-}
+} /* end of: get_response_head */
 
 
 /* read data from stream */
+/* returns amount of data readed */
 static int read_data (int sock, void *ptr, size_t size)
 {
-    int len = 0;
+    int len;
+    
+    /* wait for data */
+    if (sleep_for_data (sock))  return -1;
 
-    while (len < size) {
-	/* wait for data */
-	if (sleep_for_data (sock))  return 1;
+    len = recv (sock, ptr, size, 0);
+
+    if (len == -1 && errno == EAGAIN)
+	return 0;
+
+    return len;
+} /* end of: read_data */
+
+
+/* buffer filling thread */
+static void buffer_thread (http_desc_t *desc)
+{
+    while (desc->going) {
+	void *newbuf;
+	int readed;
 	
-	/* read */
-	if (read (sock, ptr + len, 1) <=0 ) {
-	    alsaplayer_error ("HTTP: Error reading from socket.\n");
-	    return 1;
+	printf ("len = %u\n", desc->len);
+
+	/* check for overflow */
+	if (desc->len > HTTP_BUFFER_SIZE) {
+	    dosleep (250000);
+	    continue;
 	}
 	
-	len += 1;
-    }
+	/* lock buffer ( */
+	pthread_mutex_lock (&desc->buffer_lock);
+	
+	/* enlarge buffer */
+	newbuf = malloc (desc->len + HTTP_BLOCK_SIZE);
+	memcpy (newbuf, desc->buffer, desc->len);
+	
+	/* read to buffer */
+        readed = read_data (desc->sock, newbuf+desc->len, HTTP_BLOCK_SIZE);
+	
+	if (readed == 0)
+	    desc->going = 0;
+	else if (readed <0) {
+	    desc->error = 1;
+	    desc->going = 0;
+	}
 
-    return 0;
-}
+	printf ("readed = %u, error = %u, going = %u\n", readed, desc->error, desc->going);
+	
+	/* switch buffers */
+	free (desc->buffer);
+	desc->buffer = newbuf;
+	desc->len += readed;
+
+	/* unlock buffer ) */
+	pthread_mutex_unlock (&desc->buffer_lock);
+	pthread_cond_broadcast (&desc->read_condition);
+	
+	dosleep (250000);
+    }
+    
+    pthread_exit (NULL);
+} /* end of: buffer_thread */
+
 
 /* close exist connection, and open new one */
 static int reconnect (http_desc_t *desc)
@@ -177,12 +242,30 @@ static int reconnect (http_desc_t *desc)
     struct sockaddr_in address;
     fd_set set;
     struct timeval tv;
+    int flags;
 
+    /* Clear error status */
+    desc->error = 0;
+    
+    /* Stop filling thread */
+    if (desc->going) {
+	desc->going = 0;
+	pthread_join (desc->thread, NULL);
+    }
+    
     /* Close connection */
     if (desc->sock) {
 	close (desc->sock);
 	desc->sock = 0;
     }
+
+    /* Free buffer */
+    if (desc->buffer) {
+	free (desc->buffer);
+	desc->buffer = NULL;
+    }
+    desc->begin = 0;
+    desc->len = 0;
     
     /* Look up for host IP */
     if (!(hp = gethostbyname (desc->host))) {
@@ -197,7 +280,8 @@ static int reconnect (http_desc_t *desc)
 	return 1;
     }
     
-    fcntl (desc->sock, F_SETFL, O_NONBLOCK);
+    flags = fcntl (desc->sock, F_GETFL, 0);
+    fcntl (desc->sock, F_SETFL, flags | O_NONBLOCK);
 
     /* Fill address struct */
     address.sin_family = AF_INET;
@@ -223,7 +307,7 @@ static int reconnect (http_desc_t *desc)
 	return 1;
     }
 
-    /* Test for errors while connection */
+    /* Test for errors while connected */
     error_len = sizeof (error);
     getsockopt (desc->sock, SOL_SOCKET, SO_ERROR, &error, &error_len);
     if (error) {
@@ -232,13 +316,16 @@ static int reconnect (http_desc_t *desc)
     }
 
     /* Get info about remote file */
-    snprintf (request, 2048, "HEAD %s HTTP/1.1\r\n"
+    snprintf (request, 2048, "GET %s HTTP/1.1\r\n"
 			     "Host: %s\r\n"
 			     "User-Agent: %s/%s\r\n"
+			     "Range: bytes=%ld-\r\n"
 			     "\r\n",
-			     desc->path, desc->host, PACKAGE, VERSION);
+			     desc->path, desc->host, PACKAGE, VERSION,
+			     desc->pos);
 
     write (desc->sock, request, strlen (request));
+    desc->begin = desc->pos;
  
     if (get_response_head (desc->sock, response, 10240))
 	return 1;
@@ -246,13 +333,19 @@ static int reconnect (http_desc_t *desc)
     /* Looking for size */
     s = strstr (response, "\r\nContent-Length: ");
     if (s) {
-	desc->size = atol (s+18);
+	/* Set size only once */
+	if (!desc->size)
+	    desc->size = atol (s+18);
     } else {
 	alsaplayer_error ("HTTP: Content-Length header is absent!\n");
     }
 
+    /* Attach thread to fill a buffer */
+    desc->going = 1;
+    pthread_create (&desc->thread, NULL, (void* (*)(void *)) buffer_thread, desc);
+    
     return 0;
-}
+} /* end of: reconnect */
 
 /* --------------------------------------------------------------- */
 /* ------------------------------------ PLUGIN FUNCTIONS --------- */
@@ -262,12 +355,19 @@ static void http_close(void *d)
 {
     http_desc_t *desc = (http_desc_t*)d;
     
+    /* stop buffering thread */
+    if (desc->going) {
+	desc->going = 0;
+	pthread_join (desc->thread, NULL);
+    }
+    
+    /* free resources */
     if (desc->host)  free (desc->host);
     if (desc->path)  free (desc->path);
     if (desc->sock)  close (desc->sock);
     
     free (desc);
-}
+} /* end of http_close */
 
 
 /* open stream */
@@ -280,7 +380,14 @@ static void *http_open(const char *uri)
     desc->sock = 0;
     desc->size = 0;
     desc->pos = 0;
-   
+    desc->buffer = NULL;
+    desc->begin = 0;
+    desc->len = 0;
+    desc->direction = 0;  
+    pthread_mutex_init (&desc->buffer_lock, NULL);
+    pthread_mutex_init (&desc->operation_lock, NULL);
+    pthread_cond_init (&desc->read_condition, NULL);
+ 
     /* Parse URI */
     if (parse_uri (uri, &desc->host, &desc->port, &desc->path))
 	return NULL;
@@ -323,46 +430,75 @@ static void http_shutdown()
 static size_t http_read (void *ptr, size_t size, void *d)
 {
     http_desc_t *desc = (http_desc_t*)d;
-    char request [2048];
-    char response [10240];
-    char *s;
-    
+    pthread_mutex_t mut;
+
     printf ("HTTP: Read ptr=%p, size=%d\n", ptr, size);
+ 
+    pthread_mutex_init (&mut, NULL);
     
-    /* Send request */
-    snprintf (request, 2048, "GET %s HTTP/1.1\r\n"
-			     "Host: %s\r\n"
-			     "User-Agent: %s/%s\r\n"
-			     "Range: bytes=%ld-%ld\r\n"
-			     "\r\n",
-			     desc->path, desc->host,
-			     PACKAGE, VERSION,
-			     desc->pos, desc->pos + size-1);
+    pthread_mutex_lock (&desc->operation_lock);
 
-    write (desc->sock, request, strlen (request));
+    /* check for reopen */
+    if (desc->begin > desc->pos || desc->begin + desc->len + HTTP_BLOCK_SIZE < desc->pos) {
+        printf ("HTTP: We should do reseek!!!!\n");
+	reconnect (desc);
+    }	
+ 
+    /* wait while the buffer will has entirly needed block */
+    while (1) {
+	int readed;
+	
+	/* check for error */
+	if (desc->error) {
+	    printf ("error in readdddd\n");
+	
+	    pthread_mutex_unlock (&desc->operation_lock);
 
-    /* Get head of response */
-    if (get_response_head (desc->sock, response, 10240))
-	return 0;
+	   return 0;
+	}
 
-    /* Looking for size */
-    s = strstr (response, "\r\nContent-Length: ");
-    if (s) {
-	size = atol (s+18);
-	printf ("%d\n", size);
-    } else {
-	alsaplayer_error ("HTTP: Content-Length header is absent!\n");
-	return 0;
+	readed = desc->begin + desc->len - desc->pos;
+	
+	/* done? */
+	if (readed >= size) {
+	    printf ("dooone\n");
+	    
+	    memcpy (ptr, desc->buffer + desc->pos - desc->begin, size);
+	    pthread_mutex_unlock (&desc->operation_lock);
+
+	    desc->pos += size;
+	    return size;
+	}
+
+	/* EOF reached and there is some data */
+	if (!desc->going && readed > 0) {
+	    printf ("eof reached\n");
+	    
+	    memcpy (ptr, desc->buffer + desc->pos - desc->begin, readed);
+	    pthread_mutex_unlock (&desc->operation_lock);
+
+	    desc->pos += readed;
+	    return size;
+	}
+
+	/* EOF reached and there is no data readed*/
+	if (!desc->going) {
+	    printf ("eof reached\n");
+	    
+	    pthread_mutex_unlock (&desc->operation_lock);
+
+	    return 0;
+	}
+	
+	pthread_mutex_lock (&mut);
+	pthread_cond_wait (&desc->read_condition, &mut);
+	pthread_mutex_unlock (&mut);
     }
-    
-    /* read */
-    read_data (desc->sock, ptr, size);
-    
-    /* Increment position in stream */
-    desc->pos += size;
-       
-    return size;
-}
+
+    pthread_mutex_unlock (&desc->operation_lock);
+
+    return 0;
+} /* http_read */
 
 
 /* seek in stream */
