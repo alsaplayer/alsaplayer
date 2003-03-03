f5169ad8801498b8988513419c247523440f1226
adnans <adnans@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2003-03-03 08:33:47 +0000

    New files to support OggFlac

---
+++ alsaplayer/input/flac/OggFlacStream.cpp
@@ -0,0 +1,203 @@
+//-------------------------------------------------------------------------
+//  This class encapsulates the OggFLAC stream.  Only the functions
+//  needed by the alsaplayer flac plugin are implemented.
+//
+// Copyright (c) 2002 by Drew Hess <dhess@bothan.net>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//-------------------------------------------------------------------------
+
+#include "OggFlacStream.h"
+#include "FlacEngine.h"
+
+#include <string.h>
+#include "alsaplayer_error.h"
+
+#include <string>
+
+using namespace std;
+
+namespace Flac
+{
+
+// static
+bool
+OggFlacStream::isOggFlacStream (const string & name)
+{
+  reader_type * rdr = reader_open (name.c_str ());
+  if (!rdr)
+      return false;
+
+  OggFlacStream f (name, rdr, false);
+  
+  return f.open ();
+
+} // OggFlacStream::isOggFlacStream
+
+
+OggFlacStream::OggFlacStream (const string & name,
+			      reader_type * f,
+			      bool reportErrors)
+    : FlacStream (name, f, reportErrors),
+      _decoder (0)
+{
+} // OggFlacStream::OggFlacStream
+
+
+OggFlacStream::~OggFlacStream ()
+{
+    if (_decoder)
+    {
+	OggFLAC__stream_decoder_finish (_decoder);
+	OggFLAC__stream_decoder_delete (_decoder);
+	_decoder = 0;
+    }
+
+} // OggFlacStream::~OggFlacStream
+
+
+bool
+OggFlacStream::open ()
+{
+    // it's illegal to call this on an already open stream
+    if (_decoder) 
+	return false;
+
+    _decoder = OggFLAC__stream_decoder_new ();
+    if (!_decoder) 
+	return false;
+
+    bool status = true;
+    status &= OggFLAC__stream_decoder_set_read_callback (_decoder,
+							 readCallBack);
+    status &= OggFLAC__stream_decoder_set_write_callback (_decoder, 
+							  writeCallBack);
+    status &= OggFLAC__stream_decoder_set_metadata_callback (_decoder,
+							     metaCallBack);
+    status &= OggFLAC__stream_decoder_set_error_callback (_decoder,
+							  errCallBack);
+    status &= OggFLAC__stream_decoder_set_client_data (_decoder,
+						       (void *) this);
+
+    if (!status)
+	return false;
+
+    status = (OggFLAC__stream_decoder_init (_decoder) == 
+	      OggFLAC__STREAM_DECODER_OK);
+    if (!status)
+	return false;
+
+    // this will invoke the metaCallBack
+    if (!OggFLAC__stream_decoder_process_until_end_of_metadata (_decoder))
+	return false;
+
+    // now that we've opened the stream, tell the engine it's safe to 
+    // initialize itself.
+    
+    if (!_engine->init ())
+	return false;
+
+    // return the metaCallBack's status
+    return _mcbSuccess;
+
+} // OggFlacStream::open
+
+
+bool
+OggFlacStream::processOneFrame ()
+{
+    if (!_decoder)
+	return false;
+
+    return OggFLAC__stream_decoder_process_single (_decoder);
+
+} // OggFlacStream::processOneFrame
+
+
+// static
+void
+OggFlacStream::metaCallBack (const OggFLAC__StreamDecoder * decoder,
+			     const FLAC__StreamMetadata * md,
+			     void * client_data)
+{
+    OggFlacStream * f = (OggFlacStream *) client_data;
+
+    if (!f)
+    {
+	f->apError ("OggFlacStream::metaCallBack(): no client data");
+	return;
+    }
+
+    f->realMetaCallBack (md);
+
+} // OggFlacStream::metaCallBack
+
+
+// static
+void
+OggFlacStream::errCallBack (const OggFLAC__StreamDecoder * decoder,
+			    FLAC__StreamDecoderErrorStatus status,
+			    void * client_data)
+{
+    OggFlacStream * f = (OggFlacStream *) client_data;
+    if (!f)
+    {
+	f->apError ("OggFlacStream::errCallBack (): no client data");
+	return;
+    }
+
+    f->realErrCallBack ("OggFLAC", status);
+
+} // OggFlacStream::errCallBack
+
+
+// static
+FLAC__StreamDecoderWriteStatus
+OggFlacStream::writeCallBack (const OggFLAC__StreamDecoder * decoder,
+			      const FLAC__Frame * frame,
+			      const FLAC__int32 * const buffer[],
+			      void * client_data)
+{
+    if (!client_data)
+	return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+    
+    OggFlacStream * f = (OggFlacStream *) client_data;
+    if (!f)
+	return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+
+    return f->realWriteCallBack (frame, buffer);
+    
+} // OggFlacStream::writeCallBack
+
+
+// static
+FLAC__StreamDecoderReadStatus
+OggFlacStream::readCallBack (const OggFLAC__StreamDecoder * decoder,
+			     FLAC__byte buffer[],
+			     unsigned * bytes,
+			     void * client_data)
+{
+    if (!client_data)
+	return FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+
+    OggFlacStream * f = (OggFlacStream *) client_data;
+    if (!f)
+	return FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+
+    return f->realReadCallBack (buffer, bytes);
+
+} // OggFlacStream::readCallBack
+
+} // namespace Flac
---
+++ alsaplayer/input/flac/OggFlacStream.h
@@ -0,0 +1,146 @@
+//-------------------------------------------------------------------------
+//  This class encapsulates an OggFLAC stream.  Only the functions
+//  needed by the alsaplayer flac plugin are implemented.
+//
+//  Copyright (c) 2002 by Drew Hess <dhess@bothan.net>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//-------------------------------------------------------------------------
+
+
+#ifndef _OGG_FLAC_STREAM_H_
+#define _OGG_FLAC_STREAM_H_
+
+#include <string>
+#include "reader.h"
+#include "FlacStream.h"
+
+extern "C"
+{
+#include <OggFLAC/stream_decoder.h>
+}
+
+#undef DEBUG
+
+namespace Flac
+{
+
+class FlacEngine;
+class FlacTag;
+
+class OggFlacStream : public FlacStream
+{
+ public:
+
+    //-----------------------------------------------------------------
+    // Return true if we think name points to  a valid OggFLAC stream.
+    //-----------------------------------------------------------------
+
+    static bool isOggFlacStream (const std::string & name);
+
+    
+ public:
+
+    //---------------------------------------------------------------
+    // Constructor & destructor.  The reader_type f belongs to the
+    // OggFlacStream after construction, and it will be closed upon
+    // deletion of the OggFlacStream object.  If reportErrors is
+    // false, the object will squelch all alsaplayer_error messages.
+    // This is particularly useful when attempting to open streams
+    // to determine whether they're OggFLAC streams.
+    //---------------------------------------------------------------
+
+    OggFlacStream (const std::string & name,
+		   reader_type * f,
+		   bool reportErrors = true);
+
+    virtual ~OggFlacStream ();
+
+
+    //--------------------------------------------------------
+    // See FlacStream.h for a description of these functions.
+    //--------------------------------------------------------
+
+    virtual bool open ();
+    virtual bool processOneFrame ();
+
+
+    //----------------------------------------
+    // Seeks are unsupported.  Returns false.
+    //----------------------------------------
+
+    virtual bool seekAbsolute (FLAC__uint64 sample);
+
+
+ private:
+
+    //-----------------------------------------------------------------
+    // The flac metadata callback.  It's called as a side effect of the
+    // open method.  It'll be called once for each metadata block in
+    // the flac stream.  To check its success, look at _mcbSuccess.
+    //-----------------------------------------------------------------
+
+    static void metaCallBack (const OggFLAC__StreamDecoder * decoder,
+			      const FLAC__StreamMetadata * md,
+			      void * cilent_data);
+
+    static FLAC__StreamDecoderWriteStatus 
+	writeCallBack (const OggFLAC__StreamDecoder * decoder,
+		       const FLAC__Frame * frame,
+		       const FLAC__int32 * const buffer[],
+		       void * client_data);
+
+    static FLAC__StreamDecoderReadStatus
+	readCallBack (const OggFLAC__StreamDecoder * decoder,
+		      FLAC__byte buffer[],
+		      unsigned * bytes,
+		      void * client_data);
+
+    static void errCallBack (const OggFLAC__StreamDecoder * decoder,
+			     FLAC__StreamDecoderErrorStatus status,
+			     void * client_data);
+
+
+ private:
+
+    OggFlacStream ();
+
+
+ private:
+
+    OggFLAC__StreamDecoder * _decoder;
+
+}; // class OggFlacStream
+
+
+//----------------
+// Inline methods.
+//----------------
+
+inline bool
+OggFlacStream::seekAbsolute (FLAC__uint64 sample)
+{
+    return false;
+}
+
+inline
+OggFlacStream::OggFlacStream ()
+  : _decoder (0)
+{
+}
+
+} // namespace Flac
+
+#endif // _OGG_FLAC_STREAM_H_
