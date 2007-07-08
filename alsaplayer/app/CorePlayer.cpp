6304de53feb2fb7325abfbd64eed560b6a3374a8
peter_lemenkov <peter_lemenkov@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2007-07-08 06:06:34 +0000

    cosmetic

--- alsaplayer/app/CorePlayer.cpp
+++ alsaplayer/app/CorePlayer.cpp
@@ -394,7 +394,6 @@ int CorePlayer::RegisterPlugin(input_plugin *the_plugin)
 	}
 	if (error_count) {
 	    	alsaplayer_error("At least %d error(s) were detected", error_count);
-		//Unlock();
 		return 0;
 	}
 	
@@ -410,6 +409,7 @@ int CorePlayer::RegisterPlugin(input_plugin *the_plugin)
 //			return 0;
 		}
 	}
+
 	tmp->name = the_plugin->name;
 	tmp->author = the_plugin->author;
 	tmp->init = the_plugin->init;
@@ -429,14 +429,11 @@ int CorePlayer::RegisterPlugin(input_plugin *the_plugin)
 	tmp->track_seek = the_plugin->track_seek;
 	
 	if (!tmp->init()) {
-		alsaplayer_error("Plugin failed to initialize (\"%s\")",
-				tmp->name);
+		alsaplayer_error("Plugin failed to initialize (\"%s\")", tmp->name);
 		return 0;
 	}	
 	plugin_count++;
-	if (plugin_count == 1) { // First so assign plugin
-		//plugin = tmp;
-	}
+	
 	if (global_verbose)
 		alsaplayer_error("Loading Input plugin: %s", tmp->name);
 
