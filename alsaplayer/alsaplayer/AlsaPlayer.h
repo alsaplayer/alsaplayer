5167a7dff7889917300376216e2dbd3009b20d5c
dominique_libre <dominique_libre@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2007-07-08 14:17:12 +0000

    Updated alsaplayer/* to GPL3.

--- alsaplayer/alsaplayer/AlsaPlayer.h
+++ alsaplayer/alsaplayer/AlsaPlayer.h
@@ -1,19 +1,21 @@
 /*  AlsaPlayer.h
  *  Copyright (C) 2002 Andy Lo A Foe <andy@alsaplayer.org>
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  This file is part of AlsaPlayer.
+ *
+ *  AlsaPlayer is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  AlsaPlayer is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
  */ 
 
 #ifndef __AlsaPlayer_h__
