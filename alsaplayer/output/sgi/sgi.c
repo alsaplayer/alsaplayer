55198eec6500d16197618ab4f3b7fb4254a4d25c
dominique_libre <dominique_libre@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2007-07-08 16:30:39 +0000

    Updated output/* to GPL3 + added SGI exception in COPYING.

--- alsaplayer/output/sgi/sgi.c
+++ alsaplayer/output/sgi/sgi.c
@@ -1,19 +1,25 @@
 /*  sgi.c - SGI Audio Library output module
  *  Copyright (C) 1999 Michael Pruett <michael@68k.org>
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  This file is part of AlsaPlayer.
+ *
+ *  AlsaPlayer is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  AlsaPlayer is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * As an exception to the GPL-v3 licence, you are authorised to compile,
+ * link and distribute AlsaPlayer on a SGI system. See the COPYING file
+ * in the main AlsaPlayer directory.
+ *
 */ 
 
 #include "config.h"
