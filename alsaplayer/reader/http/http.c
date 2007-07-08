836e0543d3d9f1b92287e754f00e05d8c16d20f8
dominique_libre <dominique_libre@d285aa52-1d29-0410-9d4c-b54c28f01bc6>
2007-07-08 16:38:45 +0000

    Updated reader/* to GPL3.

--- alsaplayer/reader/http/http.c
+++ alsaplayer/reader/http/http.c
@@ -2,19 +2,20 @@
  *   Copyright (C) 2002 Evgeny Chukreev <codedj@echo.ru>
  *   Copyright (C) 2002 Andy Lo A Foe <andy@alsaplayer.org>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ *  This file is part of AlsaPlayer.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ *  AlsaPlayer is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  AlsaPlayer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  *   $Id$
  *
